47b9c96151e9f5ebff00f683a8a2974d1ac5e7d5
danelynnn <danelynnn@gmail.com>
2026-07-01 13:01:38 -0600

    for some reason, clicking on the full li element for type <= 3 defaults to 7x7 easy?!!? WHAT

--- backend/app.py
+++ backend/app.py
@@ -2,8 +2,9 @@ from flask import Flask, request
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.select import Select
+from selenium.common.exceptions import WebDriverException
 
 from html.parser import HTMLParser
 
@@ -14,65 +15,68 @@ app = Flask(__name__)
 
 options = Options()
 options.page_load_strategy = 'eager'
-options.add_argument('--headless=new')
-driver = webdriver.Firefox(options=options)
+# options.add_argument('--headless=new')
+driver = webdriver.Chrome(options=options)
 
 
 def loadPuzzle(puzzleType, puzzleId):
-    global driver
-
-    if puzzleId == -1:
-        driver.get("https://www.puzzle-bridges.com")
-        
-        sizes = ["7x7-easy",
-            "7x7-normal",
-            "7x7-hard",
-            "10x10-easy",
-            "10x10-normal",
-            "10x10-hard",
-            "15x150-easy",
-            "15x15-normal",
-            "15x15-hard",
-            "25x25-easy",
-            "25x25-normal",
-            "25x25-hard",
-            None,
-            None,
-            None,
-            "7x7-dense",
-            "10x10-dense",
-            "15x15-dense",
-            "25x25-dense"]
-        menu = driver.find_element(By.ID, 'menuSizes')
-        difficulty = driver.find_element(By.CSS_SELECTOR, f"[mvvm-class*='{sizes[int(puzzleType)]}']")
-        difficulty.click()
-    else:
-        driver.get("https://www.puzzle-bridges.com/specific.php")
-
-        size = Select(driver.find_element(By.ID, 'size'))
-        size.select_by_value(puzzleType)
-
-        specId = driver.find_element(By.ID, 'specid')
-        specId.send_keys(puzzleId)
-
-        form = driver.find_element(By.CLASS_NAME, 'specific-form')
-        submit = form.find_element(By.TAG_NAME, 'puzzle-button')
-        submit.click()
-
     id = -1
     source = ""
 
     try:
-        board = driver.find_element(By.ID, 'puzzleID')
-        id = board.get_attribute('innerHTML')
+        if puzzleId == -1:
+            driver.get("https://www.puzzle-bridges.com")
+            
+            sizes = ["7x7-easy",
+                "7x7-normal",
+                "7x7-hard",
+                "10x10-easy",
+                "10x10-normal",
+                "10x10-hard",
+                "15x150-easy",
+                "15x15-normal",
+                "15x15-hard",
+                "25x25-easy",
+                "25x25-normal",
+                "25x25-hard",
+                None,
+                None,
+                None,
+                "7x7-dense",
+                "10x10-dense",
+                "15x15-dense",
+                "25x25-dense"]
+            menu = driver.find_element(By.ID, 'menuSizes')
+            difficulty = driver.find_element(By.CSS_SELECTOR, f"[mvvm-class*='{sizes[int(puzzleType)]}']")
+            link = difficulty.find_element(By.TAG_NAME, 'a')
+            # print(link.get_attribute('outerHTML'))
+            link.click()
+        else:
+            driver.get("https://www.puzzle-bridges.com/specific.php")
 
-        board = driver.find_element(By.CLASS_NAME, 'board-tasks')
-        source = board.get_attribute('outerHTML')
-    except:
-        print("error?")
-        with open('out.html', 'w') as file:
-            file.write(str(driver.page_source.encode("utf-8", errors="ignore")))
+            size = Select(driver.find_element(By.ID, 'size'))
+            size.select_by_value(puzzleType)
 
+            specId = driver.find_element(By.ID, 'specid')
+            specId.send_keys(puzzleId)
+
+            form = driver.find_element(By.CLASS_NAME, 'specific-form')
+            submit = form.find_element(By.TAG_NAME, 'puzzle-button')
+            submit.click()
+
+        try:
+            board = driver.find_element(By.ID, 'puzzleID')
+            id = board.get_attribute('innerHTML')
+
+            board = driver.find_element(By.CLASS_NAME, 'board-tasks')
+            source = board.get_attribute('outerHTML')
+        except:
+            print("error?")
+            with open('out.html', 'w') as file:
+                file.write(str(driver.page_source.encode("utf-8", errors="ignore")))
+    except WebDriverException as e:
+        print(e)
+        
     # driver.close()
     return id, source
 
@@ -116,6 +120,7 @@ class GameReader(HTMLParser):
 @app.route('/get_puzzle/<puzzleType>')
 def getPuzzle(puzzleType):
     id = request.args.get('id', -1)
+    print(puzzleType)
     id, source = loadPuzzle(puzzleType, id)
     if not source:
         return {'id': str(id), 'board': ''}
